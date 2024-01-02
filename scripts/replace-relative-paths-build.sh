ee0eb696f31d8ec9137b0284c4f7043856dc4b41
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-01-02 21:04:15 +0100

    feat: new domain

--- scripts/replace-relative-paths-build.sh
+++ scripts/replace-relative-paths-build.sh
@@ -1,10 +1,10 @@
 echo "1. Replacing relative paths"
-reprec -p '"../../static/' -i '"https://devspills.com/static/' -n -f '.*\.[md,html]' _site
-reprec -p '"../static/' -i '"https://devspills.com/static/' -n -f '.*\.[md,html]' _site
-reprec -p '"static/' -i '"https://devspills.com/static/' -n -f '.*\.[md,html]' _site
-reprec -p '"../../assets/' -i '"https://devspills.com/assets/' -n -f '.*\.[md,html]' _site
-reprec -p '"../assets/' -i '"https://devspills.com/assets/' -n -f '.*\.[md,html]' _site
-reprec -p '"assets/' -i '"https://devspills.com/assets/' -n -f '.*\.[md,html]' _site
+reprec -p '"../../static/' -i '"https://rafalschmidt.com/static/' -n -f '.*\.[md,html]' _site
+reprec -p '"../static/' -i '"https://rafalschmidt.com/static/' -n -f '.*\.[md,html]' _site
+reprec -p '"static/' -i '"https://rafalschmidt.com/static/' -n -f '.*\.[md,html]' _site
+reprec -p '"../../assets/' -i '"https://rafalschmidt.com/assets/' -n -f '.*\.[md,html]' _site
+reprec -p '"../assets/' -i '"https://rafalschmidt.com/assets/' -n -f '.*\.[md,html]' _site
+reprec -p '"assets/' -i '"https://rafalschmidt.com/assets/' -n -f '.*\.[md,html]' _site
 # reprec -p '"../' -i '"/' -n -f '.*\.[md,html]' _site # nav links
 # reprec -p '".."' -i '"/"' -n -f '.*\.[md,html]' _site #nav homepage
 echo "Replacing relative paths completed ✅"
